cad4c98e78cde8b76a7fe374ffda0c5a45f44000
janb- <janb-@LAPTOP-D76UCH1A.speedport.ip>
2022-10-30 19:37:46 +0100

    add solution

--- HW3/StateMachine_Demo/src/de/uni/koeln/se/state/Elevator.java
+++ HW3/StateMachine_Demo/src/de/uni/koeln/se/state/Elevator.java
@@ -12,13 +12,51 @@ public class Elevator{
 	{
 		this.dest_Floor=dest_floor;
 	System.out.println("*** Destination floor is: "+ dest_Floor);
-	arrive_atFloor();
-
+	this.State.Current_State = this.State.Idle;
+	System.out.println(this.State.Current_State);
+	
+	if(current_Floor > this.dest_Floor) {
+		go_down();
+	} else if( current_Floor < this.dest_Floor) {
+		go_up();
+	} else {
+		exit();
+	}
 
 	}
 
 	private void arrive_atFloor() {
 		
+		this.State.Current_State = this.State.Idle;
+		System.out.println(this.State.Current_State);
+		exit();
+		
+	}
+	
+	private void go_down() {
+		this.State.Current_State = this.State.Moving_down;
+		System.out.println(this.State.Current_State);
+		current_Floor--;
+		if(current_Floor > this.dest_Floor) {
+			go_down();
+		} else {
+			arrive_atFloor();
+		}
+	}
+	
+	private void go_up() {
+		this.State.Current_State = this.State.Moving_up;
+		System.out.println(this.State.Current_State);
+		current_Floor++;
+		if(current_Floor < this.dest_Floor) {
+			go_up();
+		} else {
+			arrive_atFloor();
+		}
+	}
+	
+	private void exit() {
+		System.out.println("Exiting elevator!");
 	}
 
 	
